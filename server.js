747689420a054988de22f6ccaee963bb6e3bf1b3
zagalib128-tech <zagalib128@gmail.com>
2026-07-06 21:58:49 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
+// Add headers to bypass FotMob blocking
+app.use((req, res, next) => {
+  req.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+  req.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8';
+  req.headers['Accept-Language'] = 'en-US,en;q=0.9';
+  req.headers['Referer'] = 'https://www.fotmob.com/';
+  req.headers['Origin'] = 'https://www.fotmob.com/';
+  next();
+});
+
 app.use("/", createProxyMiddleware({
     target: "https://www.fotmob.com",
     changeOrigin: true,
     ws: true,
-    secure: false
+    secure: true,
+    onProxyReq: (proxyReq, req, res) => {
+      proxyReq.setHeader('Host', 'www.fotmob.com');
+    }
 }));
 
 const port = process.env.PORT || 3000;
