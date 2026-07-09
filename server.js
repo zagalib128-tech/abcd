5a817f2272d27925d518b8841d3d780eb47834a4
zagalib128-tech <zagalib128@gmail.com>
2026-07-09 08:15:42 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,16 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
 app.use("/", createProxyMiddleware({
-    target: "https://www.fotmob.com",
-    changeOrigin: true,
-    ws: true,
-    secure: false,
-    xfwd: true,
-    followRedirects: true,
-    onProxyReq(proxyReq) {
-        proxyReq.setHeader("User-Agent", "Mozilla/5.0");
-        proxyReq.setHeader("Accept", "*/*");
-        proxyReq.setHeader("Accept-Language", "en-US,en;q=0.9");
-    }
+  target: "https://www.fotmob.com",
+  changeOrigin: true,
+  ws: true,
+  secure: false,
+  logLevel: "debug",
+  onError(err, req, res) {
+    console.error(err);
+    res.status(502).send("Proxy error");
+  }
 }));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("Proxy running on port " + port);
-});
-
-
+app.listen(port);
