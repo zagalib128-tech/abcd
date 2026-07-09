5886232d241da58c3fc3bc498be5fc20aa4ed665
zagalib128-tech <zagalib128@gmail.com>
2026-07-09 08:38:33 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const harmon = require("harmon");
 
 const app = express();
 
-// Images
+app.use(harmon([], [{
+    query: "html",
+    func: function (node) {
+        if (!node.createWriteStream) return;
+
+        let html = "";
+
+        const ws = node.createWriteStream();
+
+        ws._write = function (chunk, enc, next) {
+            html += chunk.toString();
+            next();
+        };
+
+        ws.on("finish", () => {
+            html = html
+                .replace(/https:\/\/images\.fotmob\.com/g, "/images")
+                .replace(/https:\/\/pub\.fotmob\.com/g, "/pub");
+
+            node.write(html);
+            node.end();
+        });
+    }
+}]));
+
 app.use("/images", createProxyMiddleware({
     target: "https://images.fotmob.com",
     changeOrigin: true,
     secure: false
 }));
 
-// Pub
 app.use("/pub", createProxyMiddleware({
     target: "https://pub.fotmob.com",
     changeOrigin: true,
     secure: false
 }));
 
-// Main FotMob
 app.use("/", createProxyMiddleware({
     target: "https://www.fotmob.com",
     changeOrigin: true,
     ws: true,
-    secure: false
+    secure: false,
+    selfHandleResponse: false
 }));
 
 const port = process.env.PORT || 3000;
